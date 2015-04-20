92d459cc4bfb58a3cc609053fd89468b6132cfe1
lw394407679 <394407679@qq.com>
2015-04-20 17:36:03 +0800

    add test
    add test

--- SoccerGo/src/com/rangers/soccergo/db/util/JsonUtil.java
+++ SoccerGo/src/com/rangers/soccergo/db/util/JsonUtil.java
@@ -67,7 +67,6 @@ public class JsonUtil {
 	public  Object json2obj(String json,Class<?> clz ){
 		try {
 			mapper = getMapper();
-		
 			return mapper.readValue(json, clz);			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
